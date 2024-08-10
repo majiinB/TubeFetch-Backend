407b1f4c5737270cc473eaec0688bfa3b47c20cf
Arthur Artugue <artuguearthur@gmail.com>
2024-08-10 23:24:48 +0800

    added download as a function, added interface in a separate file

---
+++ src/types/interfaces.ts
@@ -0,0 +1,51 @@
+
+export interface VideoFormat {
+    mimeType: string;
+    qualityLabel?: string;
+    bitrate?: number;
+    audioBitrate?: number;
+    itag: number;
+    url: string;
+    width?: number;
+    height?: number;
+    lastModified?: string;
+    quality?: string;
+    fps?: number;
+    projectionType?: string;
+    audioQuality?: string;
+    approxDurationMs?: string;
+    audioSampleRate?: string;
+    audioChannels?: number;
+    hasVideo: boolean;
+    hasAudio: boolean;
+    container?: string;
+    codecs?: string;
+    videoCodec?: string;
+    audioCodec?: string;
+    isLive?: boolean;
+    isHLS?: boolean;
+    isDashMPD?: boolean;
+}
+
+export interface VideoInfo {
+    title: String,
+    ownerChannelName: String,
+    videoId: String
+    thumbnail: {
+        url: String,
+        width: number,
+        height: number
+    }
+}
+
+export interface VideoInfoRequest {
+    url: string;
+
+}
+
+export interface InfoResponse {
+    code: string;
+    message: string;
+    videoInfo?: VideoInfo,
+    format?: VideoFormat
+}
